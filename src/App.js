d60481df7d8aa63a65d6573f226821d2e9231a41
takahashi <naotaka022@gmail.com>
2024-03-25 15:36:49 +0900

    Add quick start Rendering lists

--- src/App.js
+++ src/App.js
@@ -9,13 +9,31 @@ function MyButton() {
   );
 }
 
+const products = [
+  { title: 'Cabbage', id: 1 },
+  { title: 'Garlic', id: 2 },
+  { title: 'Apple', id: 3 },
+];
+
+const listItems = products.map(product =>
+  <li key={product.id}>
+    {product.title}
+  </li>
+);
+
+
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
 
-      <p>Creating and nesting components</p>
+      <h3>Creating and nesting components</h3>
       <MyButton />
+
+      <h3>Rendering lists</h3>
+      <ul>{listItems}</ul>
     </div>
+
+
   );
 }
