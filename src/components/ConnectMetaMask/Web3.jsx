7c785640874acb6419154ef0ccca4766c829bc76
takahashi <naotaka022@gmail.com>
2024-03-26 10:29:40 +0900

    Add sample connecting MetaMask with web3

--- src/components/ConnectMetaMask/Web3.jsx
+++ src/components/ConnectMetaMask/Web3.jsx
@@ -1,7 +1,41 @@
-export default function Web3() {
+import { useState } from 'react';
+import { Web3 } from 'web3';
+
+export default function ConnectMetaMaskWeb3() {
+  //react state to store and show the connected account
+  const [connectedAccount, setConnectedAccount] = useState('null');
+
+  async function connectMetamask() {
+    //check metamask is installed
+    if (window.ethereum) {
+      // instantiate Web3 with the injected provider
+      const web3 = new Web3(window.ethereum);
+
+      //request user to connect accounts (Metamask will prompt)
+      await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+      //get the connected accounts
+      const accounts = await web3.eth.getAccounts();
+
+      //show the first connected account in the react page
+      setConnectedAccount(accounts[0]);
+    } else {
+      alert('Please download metamask');
+    }
+  }
+
   return (
     <>
       <h1>Connect MetaMask with web3</h1>
+      <>
+        {/* Button to trigger Metamask connection */}
+        <button onClick={connectMetamask}>
+          Connect to MetaMask
+        </button>
+
+        {/* Display the connected account */}
+        <p>address: {connectedAccount}</p>
+      </>
     </>
   )
 }
